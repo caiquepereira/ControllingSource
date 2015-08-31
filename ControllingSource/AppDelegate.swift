b374fe36bac50b7c4745d69923ab59bee39a54e5
caiquepereira <caiquepereira@gmail.com>
2015-08-31 15:43:34 -0300

    teste

--- ControllingSource/AppDelegate.swift
+++ ControllingSource/AppDelegate.swift
@@ -15,7 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        //teste
+        //teste 2
+        //teste 3
         return true
     }
 
